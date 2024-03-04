83962c6d85b74438ba954e57bcbd4339eb327d91
drkknght <vitobnd79@gmail.com>
2024-03-04 23:40:04 +0530

    added key v1.0c-5

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,6 +6,7 @@ read -p "Do you wish to continue ? (yes/no) " continue_script
 if [ "$continue_script" = "yes" ]; then
 sudo pacman -S git ansible reflector
 # sudo apt install --yes ansible git
+eval $(ssh-agent -s)
 if [ ! "1" -gt "$#" ] ; then
     ansible-pull --extra-vars install_wifi="$wifi_info" -U https://github.com/drkknigt/arch-pull -vvv --ask-vault-pass --ask-become-pass -t "$(echo "$@" | tr " " ",")" --ask-pass
     exit
