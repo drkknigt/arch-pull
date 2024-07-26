0bace12b4a195641b3116fe67c580f2ebeeae462
drkknght <vitobnd79@gmail.com>
2024-07-26 09:37:27 +0530

    refactored bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,21 +1,34 @@
 #!/bin/bash
+# This files installs all essential software for my arch installation
+
+# install wifi drivers rtl8821ce
+
 read -p "Do you want to install rtl8821ce wifi driver ? (yes/no)" wifi_info
+
+
+# set up parallel downloads for pacman and update pacman local database and install git, ansible, reflector
 sudo sed -i 's/#Parallel/Parallel/g' /etc/pacman.conf
 sudo pacman -Syy
 read -p "Do you wish to continue ? (yes/no) " continue_script
 if [ "$continue_script" = "yes" ]; then
 sudo pacman -S git ansible reflector
-# sudo apt install --yes ansible git
+
+# start ssh agent in background
 eval $(ssh-agent -s)
-if [ ! "1" -gt "$#" ] ; then
+
+# run this if script passed with ansible tags 
+if [ "$#" -gt "0" ] ; then
     ansible-pull --extra-vars install_wifi="$wifi_info" -U https://github.com/drkknigt/arch-pull -vvv --ask-vault-pass --ask-become-pass -t "$(echo "$@" | tr " " ",")" --ask-pass
     exit
 fi
-#
+
+# ansible pull install everything including wifi
 if [ "$wifi_info" = "yes" ]; then
     ansible-pull --extra-vars install_wifi="$wifi_info" -U https://github.com/drkknigt/arch-pull -vvv --ask-become-pass --ask-pass
 else
+# ansible pull install everything except wifi
     ansible-pull --extra-vars install_wifi="no" -U https://github.com/drkknigt/arch-pull -vvv --ask-become-pass --ask-pass
 fi
+# set default applications
 . ~/.dotfiles/sys_d/systemd-disabled
 fi
